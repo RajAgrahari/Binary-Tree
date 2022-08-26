0636fc2975e5e6964efc085ddcb1010646a16331
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2022-08-26 23:09:39 +0530

    DAY-8

---
+++ ConstructBSTfromPreorder.cpp
@@ -0,0 +1,44 @@
+// geeksforgeeks.org/construct-bst-from-given-preorder-traversa/
+struct node* constructTreeUtil(int pre[], int* preIndex,
+                               int low, int high, int size)
+{
+    // Base case
+    if (*preIndex >= size || low > high)
+        return NULL;
+ 
+    // The first node in preorder traversal is root. So take
+    // the node at preIndex from pre[] and make it root, and
+    // increment preIndex
+    struct node* root = newNode(pre[*preIndex]);
+    *preIndex = *preIndex + 1;
+ 
+    // If the current subarray has only one element, no need
+    // to recur
+    if (low == high)
+        return root;
+ 
+   // Search for the first element greater than root
+    int i;
+    for (i = low; i <= high; ++i)
+        if (pre[i] > root->data)
+            break;
+ 
+    // Use the index of element found in preorder to divide
+    // preorder array in two parts. Left subtree and right
+    // subtree
+    root->left = constructTreeUtil(pre, preIndex, *preIndex,
+                                   i - 1, size);
+    root->right
+        = constructTreeUtil(pre, preIndex, i, high, size);
+ 
+    return root;
+}
+ 
+// The main function to construct BST from given preorder
+// traversal. This function mainly uses constructTreeUtil()
+struct node* constructTree(int pre[], int size)
+{
+    int preIndex = 0;
+    return constructTreeUtil(pre, &preIndex, 0, size - 1,
+                             size);
+}
